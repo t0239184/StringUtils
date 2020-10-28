c777e74ee29ce032ff07e6480c2f07000e433e3a
Alan Chen <alanchen@cherricorp.com>
2020-10-28 22:51:25 +0800

    Update test case

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
-#include "StringUtils.h"
-#include <string>
 #include <iostream>
+#include <string>
+
+#include "StringUtils.h"
 
 using namespace std;
 
 int main() {
-
     string str;
-    bool isEmpty = StringUtils::isEmpty(str);
-    cout << isEmpty << "\n";
+    bool isEmpty;
+    bool isBlank;
 
+    /* NULL Test */
+    isEmpty = StringUtils::isEmpty(str);
+    isBlank = StringUtils::isBlank(str);
+    cout << "str=[NULL]\n";
+    cout << "isEmpty: " << isEmpty << "\n";
+    cout << "isBlank: " << isBlank << "\n";
+    cout << "\n";
 
+    /* Has value Test */
+    str = "John Doe";
+    isEmpty = StringUtils::isEmpty(str);
+    isBlank = StringUtils::isBlank(str);
+    cout << "str=[" << str << "]\n";
+    cout << "isEmpty: " << isEmpty << "\n";
+    cout << "isBlank: " << isBlank << "\n";
+    cout << "\n";
+
+    /* All space char Test */
+    str = "   ";
+    isEmpty = StringUtils::isEmpty(str);
+    isBlank = StringUtils::isBlank(str);
+    cout << "str=[" << str << "]\n";
+    cout << "isEmpty: " << isEmpty << "\n";
+    cout << "isBlank: " << isBlank << "\n";
+    cout << "\n";
 }
